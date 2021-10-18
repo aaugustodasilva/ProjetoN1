fac2a74217da0862a082a6a63213180ef687ac44
aaugustodasilva <64340172+aaugustodasilva@users.noreply.github.com>
2021-10-18 17:17:55 -0300

    Create projetoN1.py

---
+++ projetoN1.py
@@ -0,0 +1,15 @@
+listaDeNomes = []
+listaDeNomes = list
+listaDeNomes = listaDeNomes.append(input('digite seu nome:'))
+
+
+listaDeEmails = []
+listaDeEmails = list 
+listaDeEmails = listaDeEmails.append(input('digite seu email:'))
+def main():
+print(listaDeEmails)
+print(listaDeNomes)
+
+desvio condicional padrão
+if name == "main":
+main()
