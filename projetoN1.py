dd3d049cf0ff5062b105b857127bba84a1c0f49a
aaugustodasilva <64340172+aaugustodasilva@users.noreply.github.com>
2021-10-18 17:40:13 -0300

    Update projetoN1.py

--- projetoN1.py
+++ projetoN1.py
@@ -5,7 +5,7 @@ listaDeNomes = listaDeNomes.append(input('digite seu nome:'))
 
 listaDeEmails = []
 listaDeEmails = list 
-listaDeEmails = listaDeEmails.append(input('digite seu email:'))aaaaaaa
+listaDeEmails = listaDeEmails.append(input('digite seu email:'))
 def main():
 print(listaDeEmails)
 print(listaDeNomes)
