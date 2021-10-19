7e9449b460b13011f9151a9711bdb08ff34b7fa5
aaugustodasilva <64340172+aaugustodasilva@users.noreply.github.com>
2021-10-18 23:54:59 -0300

    Update projetoN1.py
    Altera_programa_final

--- projetoN1.py
+++ projetoN1.py
@@ -1,25 +1,43 @@
+def mostrarMenu():
+    print("--------------Sistema de Cadastro de Alunos---------------")
+    print("1 - Cadastrar Nome e Emails: ")
+    print("2 - Exibir cadastrados por ordem de cadastro: ")
+    print("3 - Exibir cadastrados por ordem Alfabética: ")
+    print("4 - Buscar se usuário faz parte de uma lista: ")
+    print("5 - Remover usuário: ")
+    print("6 - Alterar nome de usuário: ")
+    print("7 - Sair: ")
+
+# criando uma lista de nomes vazia
 listaDeNomes = []
-listaDeNomes = list
-listaDeNomes = listaDeNomes.append(input('Digite seu nome:\n'))
 
+# criando uma lista de e-mails vazia
 listaDeEmails = []
-listaDeEmails = list 
-listaDeEmails = listaDeEmails.append(input('Digite seu email:\n'))
+
+def cadastro_nome():
+    listaDeNomes = list
+    listaDeNomes = listaDeNomes.append(input('Digite seu nome:\n'))
+
+def cadastro_email():
+    listaDeEmails = list 
+    listaDeEmails = listaDeEmails.append(input('Digite seu email:\n'))
+
+def ordem_alfabetica():
+    print(sorted(listaDeEmails))
+    print(sorted(listaDeNomes))
+
+def usuario_parte_lista():
+    nome = input('Digite um nome especifico:')
+    print(listaDeNomes. index(nome))
+
+def email_parte_lista():
+    email = input('Digite um e-mail especifico:')
+    print(listaDeEmails. index(email))
+
 def main():
-  print(listaDeEmails)
-  print(listaDeNomes)
+    mostrarMenu()
+    print(listaDeEmails)
+    print(listaDeNomes)
 
-if name == "main":
+if( __name__ == "__main__"):
     main()
-
-print(len(listaDeEmails))
-print(len(listaDeNomes))
-
-print(sorted(listaDeEmails))
-print(sorted(listaDeNomes))
-
-nome = input('Digite um nome especifico:')
-print(listaDeNomes. index(nome))
-
-email = input('Digite um nome especifico:')
-print(listaDeEmails. index(email))
