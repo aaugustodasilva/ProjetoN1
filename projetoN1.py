812b47dbde8d7672beef911f20b9b7cf2cb33475
aaugustodasilva <64340172+aaugustodasilva@users.noreply.github.com>
2021-10-25 17:24:00 -0300

    Alteração Nova - 25-out
    Alteração do código.

--- projetoN1.py
+++ projetoN1.py
@@ -22,6 +22,9 @@ def cadastro_email():
     listaDeEmails = list 
     listaDeEmails = listaDeEmails.append(input('Digite seu email:\n'))
 
+def ordem_cadastro():
+    print("Lista de nomes por ordem de cadastro: " + listaDeNomes +"")
+
 def ordem_alfabetica():
     print(sorted(listaDeEmails))
     print(sorted(listaDeNomes))
@@ -34,10 +37,42 @@ def email_parte_lista():
     email = input('Digite um e-mail especifico:')
     print(listaDeEmails. index(email))
 
+def escolher_menu():
+    return int(input())
+
+def remover_nome():
+    print(listaDeNomes)
+    Nome_Remover = input("Escolha o nome a ser removido:")
+    if(Nome_Remover == listaDeNomes.index(Nome_Remover)):
+        listaDeNomes.pop(Nome_Remover)
+    else:
+        print("Nome não existente")
+
+def alterar_nome():
+    Nome_Novo = print = input("Digite o nome para substituir:")
+    print(listaDeNomes)
+    Indice_Alterar = int(input("Escolha o nome a ser alterado: Obs.: o primeiro começa com 0"))
+    listaDeNomes[Indice_Alterar] = Nome_Novo
+    print(listaDeNomes)
+
 def main():
     mostrarMenu()
-    print(listaDeEmails)
-    print(listaDeNomes)
+    opcaoEscolhida = escolher_menu()
+
+    while (opcaoEscolhida != 7):
+        if(opcaoEscolhida == 1):
+            cadastro_nome(listaDeNomes)
+            cadastro_email(listaDeEmails)
+        elif(opcaoEscolhida ==2):
+            ordem_cadastro(listaDeNomes)
+        elif(opcaoEscolhida ==3):
+            ordem_alfabetica(listaDeNomes)
+        elif(opcaoEscolhida ==4):
+            usuario_parte_lista(listaDeNomes)
+        elif(opcaoEscolhida ==5):
+            remover_nome(listaDeNomes)
+        elif(opcaoEscolhida ==6):
+            alterar_nome(listaDeNomes)
 
 if( __name__ == "__main__"):
     main()
